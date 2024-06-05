55e2e4cc32cea690081c4ded7891b2107f0da37b
Pawel <pawel.werbowy@gmail.com>
2024-06-05 10:54:46 +0100

    Create car.js

---
+++ backend/models/car.js
@@ -0,0 +1,8 @@
+const mongoose = require("mongoose");
+
+const carSchema = new mongoose.Schema({
+  model: { type: String, required: true },
+  make: { type: String, required: true },
+  registrationNumber: { type: String, required: true },
+  currentOwner: { type: String, required: true },
+});
