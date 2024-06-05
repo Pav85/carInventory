4f80c78fa24ca179d0249d052a8b35ed3510f8e6
Pawel <pawel.werbowy@gmail.com>
2024-06-05 10:47:54 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,22 @@ require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(bodyParser.json());
+app.use(cors());
+
+mongoose.connect(
+  process.env.MONGO_URI,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => {
+    console.log("Connected to MongoDB");
+  }
+);
+
+const db = mongoose.connection;
+db.once("open", () => {
+  console.log("Database connection established");
+});
+
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
