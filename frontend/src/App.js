8dba5aa53ec6d9dcb4ca1f9fad788482b9507dc1
Pawel <pawel.werbowy@gmail.com>
2024-06-06 10:37:24 +0100

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import "./App.css";
 
 function App() {
   const [cars, setCars] = useState([]);
@@ -41,7 +42,7 @@ function App() {
   };
 
   return (
-    <div>
+    <div className="App">
       <h1>Car Inventory</h1>
       <input
         type="text"
