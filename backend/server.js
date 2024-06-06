e944254b25a1d9aed7cdbfc75d8fc17c5b6dcedf
Pawel <pawel.werbowy@gmail.com>
2024-06-06 09:40:36 +0100

    mongo db connect

--- backend/server.js
+++ backend/server.js
@@ -4,24 +4,25 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 require("dotenv").config();
 
+const carRoutes = require("./routes/carRoutes");
+
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(cors());
 
-mongoose.connect(
-  process.env.MONGO_URI,
-  { useNewUrlParser: true, useUnifiedTopology: true },
-  () => {
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("Connected to MongoDB");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error.message);
+    process.exit(1);
   }
-);
+};
 
-const db = mongoose.connection;
-db.once("open", () => {
-  console.log("Database connection established");
-});
+connectDB();
 
 app.get("/", (req, res) => {
   res.send("Hello World");
