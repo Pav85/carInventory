c5c7cae6b58df2f103ef75a22631563a6f28c225
Pawel <pawel.werbowy@gmail.com>
2024-06-10 12:28:14 +0100

    Update car.js

--- backend/models/car.js
+++ backend/models/car.js
@@ -1,8 +1,8 @@
-const mongoose = require("mongoose");
-const shortid = require("shortid");
+const mongoose = require("mongoose"); // import mongoose module
+const shortid = require("shortid"); // import shortid module
 
 const carSchema = new mongoose.Schema({
-  carId: { type: String, unique: true, default: shortid.generate },
+  carId: { type: String, unique: true, default: shortid.generate }, // generate a unique carId for each car
   make: { type: String, required: true },
   model: { type: String, required: true },
   registrationNumber: { type: String, required: true },
@@ -10,4 +10,4 @@ const carSchema = new mongoose.Schema({
   year: { type: Number, required: true },
 });
 
-module.exports = mongoose.model("Car", carSchema);
+module.exports = mongoose.model("Car", carSchema); // export Car model
