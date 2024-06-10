809eb7bfccfd3cd31f4ead11158f38661ac0dabd
Pawel <pawel.werbowy@gmail.com>
2024-06-10 12:17:14 +0100

    comments added

--- backend/routes/carRoutes.js
+++ backend/routes/carRoutes.js
@@ -1,12 +1,12 @@
-const express = require("express");
-const router = express.Router();
-const carController = require("../controllers/carController");
+const express = require("express"); // import express module
+const router = express.Router(); // import router from express module
+const carController = require("../controllers/carController"); // import car controller
 
-router.post("/", carController.addCar);
-router.put("/:id", carController.updateCar);
-// router.put("/", carController.updateMultipleCars);
-router.delete("/:id", carController.deleteCar);
-router.get("/", carController.getAllCars);
-router.get("/old", carController.getOldCars);
+router.post("/", carController.addCar); // add car route
+router.put("/:id", carController.updateCar); // update car route
+// router.put("/", carController.updateMultipleCars); // update multiple cars route in the future development
+router.delete("/:id", carController.deleteCar); // delete car route
+router.get("/", carController.getAllCars); // get all cars route
+router.get("/old", carController.getOldCars); // get old cars route
 
 module.exports = router;
