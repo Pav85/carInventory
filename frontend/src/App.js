4c22aab90e861a0e13a7dc1602bbfcb90a6eb1f6
Pawel <pawel.werbowy@gmail.com>
2024-06-10 12:41:07 +0100

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,27 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
+import axios from "axios"; // Import axios for making HTTP requests
 import "./App.css";
-import { capitaliseFirstLetter } from "./utils/Capitalise";
+import { capitaliseFirstLetter } from "./utils/Capitalise"; // Import capitaliseFirstLetter function
 
+// Function to fetch all cars from the API by its short ID
 const fetchCarByShortId = async (carId) => {
   try {
     const response = await axios.get(
       `http://localhost:5000/api/cars?carId=${carId}`
     );
     if (response.data && response.data.length > 0) {
-      return response.data[0];
+      return response.data[0]; // Return the first car from the response
     } else {
       throw new Error("Car not found");
     }
   } catch (error) {
-    console.error("Error fetching car by short ID:", error);
+    console.error("Error fetching car by short ID:", error); // Log error to the console
     return null;
   }
 };
 
 function App() {
+  // state variables to manage car data and form data
   const [cars, setCars] = useState([]);
   const [newCar, setNewCar] = useState({
     make: "",
@@ -39,10 +41,12 @@ function App() {
   });
   const [showOldCars, setShowOldCars] = useState(false);
 
+  // fetch cars from the server when the component mounts
   useEffect(() => {
     fetchCars();
   }, []);
 
+  // function to fetch all cars from the server
   const fetchCars = async () => {
     try {
       console.log("Fetching cars from the server...");
@@ -54,6 +58,7 @@ function App() {
     }
   };
 
+  // function to fetch old cars from the server
   const fetchOldCars = async () => {
     try {
       const response = await axios.get("http://localhost:5000/api/cars/old");
@@ -63,6 +68,7 @@ function App() {
     }
   };
 
+  // toggle cars view to show old cars or all cars
   const toggleCarsView = () => {
     if (showOldCars) {
       fetchCars();
@@ -72,6 +78,7 @@ function App() {
     setShowOldCars(!showOldCars);
   };
 
+  // function to add a new car
   const addCar = async () => {
     try {
       await axios.post("http://localhost:5000/api/cars", newCar);
@@ -82,16 +89,17 @@ function App() {
         registrationNumber: "",
         currentOwner: "",
         year: 0,
-      });
+      }); // Reset the new car form fields
     } catch (error) {
-      console.error("Error adding car:", error);
+      console.error("Error adding car:", error); // Log error to the console
     }
   };
 
+  //  function to update an existing car
   const handleUpdateCar = async () => {
     if (!updateCar.carId) {
-      console.error("Car ID is required to update a car");
-      alert("Car ID is required to update a car");
+      console.error("Car ID is required to update a car"); // Log error to the console
+      alert("Car ID is required to update a car"); // Show alert message
       return;
     }
     try {
@@ -102,7 +110,7 @@ function App() {
             ([key, value]) => value && key !== "carId"
           )
         );
-        console.log("Updating car with data:", updatedFields); // Log the data
+        console.log("Updating car with data:", updatedFields); // Log updated fields to the console
         await axios.put(
           `http://localhost:5000/api/cars/${carDetails._id}`,
           updatedFields
@@ -116,7 +124,7 @@ function App() {
           registrationNumber: "",
           currentOwner: "",
           year: 0,
-        });
+        }); // Reset the update car form fields
       } else {
         console.error("Car not found with the provided short ID");
       }
@@ -125,6 +133,7 @@ function App() {
     }
   };
 
+  // function to delete a car
   const handleDeleteCar = async (id) => {
     try {
       await axios.delete(`http://localhost:5000/api/cars/${id}`);
@@ -138,7 +147,9 @@ function App() {
     <div className="App container mx-auto p-4">
       <h1 className="mb-3">Car Inventory</h1>
       <div className="mb-8">
+        {/* add car section */}
         <h2 className="font-semibold mb-2">Add Car</h2>
+        {/* input fields for a new car */}
         <input
           className="border p-2 mr-2 mb-2"
           type="text"
@@ -180,12 +191,15 @@ function App() {
             setNewCar({ ...newCar, year: parseInt(e.target.value, 10) })
           }
         />
+        {/* button to add a new car */}
         <button className="bg-blue-500 text-white p-2 rounded" onClick={addCar}>
           Add Car
         </button>
       </div>
       <div className="mb-8">
+        {/* update car section */}
         <h2 className="font-semibold mb-2">Update Car</h2>
+        {/* input fields for an existing car update */}
         <input
           className="border p-2 mr-2 mb-2"
           type="text"
@@ -241,6 +255,7 @@ function App() {
             setUpdateCar({ ...updateCar, year: parseInt(e.target.value, 10) })
           }
         />
+        {/* button to update an existing car */}
         <button
           className="bg-green-500 text-white p-2 rounded"
           onClick={handleUpdateCar}
@@ -248,13 +263,16 @@ function App() {
           Update Car
         </button>
       </div>
-      <h2 className="font-semibold mb-2">All Cars</h2>
+      {/* display cars section */}
+      <h2 className="font-semibold mb-2">Cars</h2>
+      {/* button to toggle all and old cars */}
       <button
         className="bg-gray-500 text-white p-2 rounded mb-4"
         onClick={toggleCarsView}
       >
         {showOldCars ? "Show All Cars" : "Show Cars Older Than 5 Years"}
       </button>
+      {/* car container rendered dynamically */}
       <div className="cars-container">
         {cars.map((car) => (
           <div key={car._id} className="car-box">
